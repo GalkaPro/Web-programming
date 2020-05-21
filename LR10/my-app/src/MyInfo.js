b029c7b45dee5e46aa459a38be97a8640fe3a9ea
Vadym Zmiievskyi <vadym.zmiievskyi@gmail.com>
2020-05-22 02:44:46 +0300

    LR12

---
+++ LR10/my-app/src/MyInfo.js
@@ -0,0 +1,19 @@
+import React from "react"
+import ReactDOM from "react-dom"
+function MyInfo() {
+    return (
+<div>
+<h1>Zmiievskyi Vadym</h1>
+<p>Меня зовут Змиeвской Вадим.Я родился 9 декабря</p>
+      <p>Живу в городе Харькове. Люблю заниматься спортом. Также нравиться путишевствовать, єто очень интересно ,весело и позновательно .</p>
+<p>Страны в которых я хочу побывать:</p>
+    <ul>
+      <li>Америка</li>
+      <li>Германия</li>
+      <li>Швеция</li>
+</ul>
+</div>
+        )
+}
+
+export default MyInfo
