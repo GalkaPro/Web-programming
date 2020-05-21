f315ff42bc4ad514ca710b344b3b4aa3c853c8c5
Alexandr <qazwsxua98@gmail.com>
2020-05-21 20:27:51 +0300

    Laboratory 10

--- Prisyazhnyuk/laboratory9.1/js/pq.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-	$('span.pq').each(function () {
-	var quote=$(this).clone();
-	quote.removeClass('pq');
-	quote.addClass('pullquote');
-	$(this).before(quote);
-	}); //end each
-	}); //end ready
